11ed7ba2b717aaf45804ff9380239d14dc7bcae0
tamer2025almstkshf <you@example.com>
2026-01-21 02:23:17 +0400

    guthub

--- components/PageHelmet.tsx
+++ components/PageHelmet.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import React, { useEffect } from 'react';
 
 interface PageHelmetProps {
   title: string;
   description: string;
 }
 
-export const PageHelmet: React.FC<PageHelmetProps> = ({ title, description }) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-  </Helmet>
-);
+export const PageHelmet: React.FC<PageHelmetProps> = ({ title, description }) => {
+  useEffect(() => {
+    document.title = title;
+
+    let meta = document.querySelector('meta[name="description"]');
+    if (!meta) {
+      meta = document.createElement('meta');
+      meta.setAttribute('name', 'description');
+      document.head.appendChild(meta);
+    }
+    meta.setAttribute('content', description);
+  }, [title, description]);
+
+  return null;
+};
